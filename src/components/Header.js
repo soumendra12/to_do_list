080718f8d005620cd6756c345698a572dcbc2816
Soumendrasamsr@LAPTOP-EFVB073H <sam.srimu@gmail.com>
2024-03-13 08:10:45 +0530

    header arranged

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,15 @@
 const Header =()=>{
 
 return(
-    <>
-    Simple To-Do List
-    </>
+    <header className="row bg">
+        <div className="col-6">
+            <img style={{height: "150px"}} src="./images_logo/to_do_logo.jpeg" alt="to_do_logo"/>
+        </div>
+        <div className="col-6 mt-5">
+TO DO List
+        </div>
+
+    </header>
 );
 
 }
